75088a0694292b59e6b5ecea1c209666b508b6d3
PaulHaze <dev@artemist.com.au>
2024-07-23 09:31:33 +1000

    add logo for navbar

--- src/components/header/nav/NavBar.tsx
+++ src/components/header/nav/NavBar.tsx
@@ -1,4 +1,11 @@
 'use client';
+import { useEffect, useState } from 'react';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { usePathname } from 'next/navigation';
+
+import { useUiContext } from '@/context/UiContext';
 
 import { ThemeToggle } from './ThemeToggle';
 
