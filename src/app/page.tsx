84cca3b578adeebed29af72b7eda3ed6889152a4
PaulHaze <dev@artemist.com.au>
2024-08-20 07:58:32 +1000

    pre-refactor commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 // import Image from 'next/image';
 
-import { HeaderOne, MobileMenu } from '@/components/header';
+import { HeaderOne } from '@/components/header';
 
 export default function Home() {
   return (
@@ -9,11 +9,6 @@ export default function Home() {
       <div>
         <div className="flex h-[400px] flex-col items-center justify-center pt-10">
           <h1 className="font-body">Holaa Movie App</h1>
-        </div>
-        <div className="font-header bg-base-300">
-          <div className="flex justify-center pt-5 text-center">
-            <MobileMenu />
-          </div>
         </div>
       </div>
     </main>
