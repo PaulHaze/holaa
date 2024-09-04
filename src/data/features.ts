9fcc9fbd2b21ae660f890f9d85fe447d8270e9a3
PaulHaze <dev@artemist.com.au>
2024-09-04 12:28:36 +1000

    fix naming of icons

---
+++ src/data/features.ts
@@ -0,0 +1,34 @@
+import featherIconOne from '@/assets/images/icons/play.svg';
+import featherIconTwo from '@/assets/images/icons/thumbs_up.svg';
+import featherIconThree from '@/assets/images/icons/double_circle.svg';
+import featherIconFour from '@/assets/images/icons/double_play.svg';
+
+export const liveStreamingFeatureData = {
+  title: 'HOLAA IS OFFERS SUPERIOR LIVE TV STREAMING',
+  features: [
+    {
+      image: featherIconOne,
+      title: 'Flexible Streaming',
+      description:
+        'Flexible streaming allows users to use customize their viewing experience',
+    },
+    {
+      image: featherIconTwo,
+      title: 'Super fast quality',
+      description:
+        'Flexible streaming allows users to use customize their viewing experience',
+    },
+    {
+      image: featherIconThree,
+      title: 'Watch from anywhere',
+      description:
+        'Flexible streaming allows users to use customize their viewing experience',
+    },
+    {
+      image: featherIconFour,
+      title: 'Download and go',
+      description:
+        'Flexible streaming allows users to use customize their viewing experience',
+    },
+  ],
+};
