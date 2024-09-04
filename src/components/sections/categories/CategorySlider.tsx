bcbf550b72f806a35b18508fd7dcb64da624d491
PaulHaze <dev@artemist.com.au>
2024-09-04 14:05:36 +1000

    add placeholders for remaining sections

---
+++ src/components/sections/categories/CategorySlider.tsx
@@ -0,0 +1,7 @@
+export function CategorySlider() {
+  return (
+    <div>
+      <p>CategorySlider Placeholder</p>
+    </div>
+  );
+}
---
+++ src/components/sections/topten/TopTen.tsx
@@ -0,0 +1,7 @@
+export function TopTen() {
+  return (
+    <div>
+      <p>TopTen Placeholder</p>
+    </div>
+  );
+}
