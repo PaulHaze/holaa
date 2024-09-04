3ad80459dca614a5b59bc1efb13da8e344a3928c
PaulHaze <dev@artemist.com.au>
2024-09-04 17:20:04 +1000

    update exports from sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
 import { HeaderOne } from '@/components/sections/header';
 import { TrendingNow } from '@/components/sections/trending';
-
 import { FeatureCards } from '@/components/sections/features/FeatureCards';
+import {} from '';
 
 export default function Home() {
   return (
     <main>
       <HeaderOne isHero={true} data={null} />
       <TrendingNow />
-
       <FeatureCards />
     </main>
   );
---
+++ src/components/sections/index.ts
@@ -0,0 +1,7 @@
+export * from './categories/CategorySlider';
+export * from './hero/HeroOne';
+export * from './features/FeatureCards';
+export * from './footer/Footer';
+export * from './header/HeaderOne';
+export * from './topten/TopTen';
+export * from './trending/TrendingNow';
