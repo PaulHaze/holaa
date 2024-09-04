c18b4b10ba6996d01dc2138c1d803d8ca2621944
PaulHaze <dev@artemist.com.au>
2024-09-04 13:50:02 +1000

    style fixes to feature cards

--- src/components/cards/FeatureCard.tsx
+++ src/components/cards/FeatureCard.tsx
@@ -6,14 +6,14 @@ type FeatureCardProps = {
 
 export function FeatureCard({ feature }: FeatureCardProps) {
   return (
-    <div className="bg-feature-card group flex flex-col items-center gap-4 rounded-[30px] py-8 transition-all duration-400">
+    <div className="bg-feature-card group flex flex-col items-center gap-4 rounded-[30px] px-10 py-8 transition-all duration-400 sm:px-4">
       <Image
         src={feature.image}
         alt="icon"
         className="pb-1 transition-all duration-400 group-hover:scale-125"
       />
-      <h5 className="uppercase">{feature.title}</h5>
-      <p className="opacity-80">{feature.description}</p>
+      <h5 className="mt-1 text-center uppercase lg:text-xl">{feature.title}</h5>
+      <p className="text-center opacity-80">{feature.description}</p>
     </div>
   );
 }
--- src/components/sections/features/FeatureCards.tsx
+++ src/components/sections/features/FeatureCards.tsx
@@ -4,17 +4,13 @@ import { featuresData as data } from '@/data/features';
 
 export function FeatureCards() {
   return (
-    <div className="w-full rounded-[40px] bg-black/50 px-4 py-10">
+    <div className="w-full rounded-[40px] bg-black/50 px-4 py-10 sm:px-10 md:px-20">
       <h3 className="mb-5 text-center">{data.title}</h3>
-      <div className="grid grid-cols-1 gap-4">
+      <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
         {data.features.length &&
           data.features.map((feature, index) => (
             <FeatureCard key={index} feature={feature} />
           ))}
-        {/* <FeatureCard feature={data.features[0]} />
-        <FeatureCard feature={data.features[1]} />
-        <FeatureCard feature={data.features[2]} />
-        <FeatureCard feature={data.features[3]} /> */}
       </div>
     </div>
   );
