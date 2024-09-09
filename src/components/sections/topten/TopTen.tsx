c5337b2e39d1f3fde2708bb6d9a0dad4a175427b
PaulHaze <dev@artemist.com.au>
2024-09-09 11:41:27 +1000

    top ten component finished

--- src/components/sections/topten/TopTen.tsx
+++ src/components/sections/topten/TopTen.tsx
@@ -1,18 +1,59 @@
 'use client';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+import type { SwiperOptions } from 'swiper/types';
+
 import { MovieCard } from '@/components/cards';
 
-// import { Swiper, SwiperSlide } from 'swiper/react';
-// import type { SwiperOptions } from 'swiper/types';
-
-import { popularMoviesData } from '@/data/movie';
+import { popularMoviesData as movies } from '@/data/movie';
 
 export function TopTen() {
+  const topTenSwiperOptions: SwiperOptions = {
+    speed: 12000,
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1,
+      disableOnInteraction: true,
+    },
+    modules: [Autoplay],
+    breakpoints: {
+      0: {
+        slidesPerView: 1.2,
+      },
+      576: {
+        slidesPerView: 1.7,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 2.5,
+        spaceBetween: 20,
+      },
+      1366: {
+        slidesPerView: 3.5,
+        spaceBetween: 30,
+      },
+      1600: {
+        slidesPerView: 3.9,
+        spaceBetween: 30,
+      },
+    },
+  };
   return (
-    <div className="pt-14">
-      <h2 className="text-center">Top Ten Shows: AUSTRALIA</h2>
+    <div className="px-10 pt-14">
+      <h2 className="mb-10 text-center">Top Ten Shows: AUSTRALIA</h2>
       <div className="top-ten">
-        <MovieCard movie={popularMoviesData[0]} />
+        {movies.length && (
+          <Swiper {...topTenSwiperOptions} className="swiper">
+            {movies.map((movie, index) => (
+              <SwiperSlide key={index}>
+                <MovieCard movie={movie} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        )}
       </div>
     </div>
   );
