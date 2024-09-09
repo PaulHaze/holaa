a35c9d82f581940c0d355e8cdd17e6f5500febba
PaulHaze <dev@artemist.com.au>
2024-09-09 10:39:07 +1000

    add first movie card

--- src/components/sections/topten/TopTen.tsx
+++ src/components/sections/topten/TopTen.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { MovieCard } from '@/components/cards';
+
 // import { Swiper, SwiperSlide } from 'swiper/react';
 // import type { SwiperOptions } from 'swiper/types';
+
+import { popularMoviesData } from '@/data/movie';
 
 export function TopTen() {
   return (
     <div className="pt-14">
       <h2 className="text-center">Top Ten Shows: AUSTRALIA</h2>
+      <div className="top-ten">
+        <MovieCard movie={popularMoviesData[0]} />
+      </div>
     </div>
   );
 }
