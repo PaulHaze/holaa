38e8ad0e680787afc6a8ed0182ecbdd76e6d80e0
PaulHaze <dev@artemist.com.au>
2024-09-09 15:53:08 +1000

    create category data

---
+++ src/data/category.ts
@@ -0,0 +1,39 @@
+import categoryImageOne from '@/assets/images/category/category-1.webp';
+import categoryImageTwo from '@/assets/images/category/category-2.webp';
+import categoryImageThree from '@/assets/images/category/category-3.webp';
+import categoryImageFour from '@/assets/images/category/category-4.webp';
+import categoryImageFive from '@/assets/images/category/category-5.webp';
+import categoryImageSix from '@/assets/images/category/category-6.webp';
+
+export const categories: Category[] = [
+  {
+    image: categoryImageOne,
+    title: 'Comedy',
+    href: 'category/comedy',
+  },
+  {
+    image: categoryImageTwo,
+    title: 'Drama',
+    href: 'category/drama',
+  },
+  {
+    image: categoryImageThree,
+    title: 'Action',
+    href: 'category/action',
+  },
+  {
+    image: categoryImageFour,
+    title: 'Kids',
+    href: 'category/kids',
+  },
+  {
+    image: categoryImageFive,
+    title: 'Animated',
+    href: 'category/animated',
+  },
+  {
+    image: categoryImageSix,
+    title: 'Classic',
+    href: 'category/classic',
+  },
+];
