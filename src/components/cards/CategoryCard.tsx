95675e46a9fb5f29af7629dcc03a11631a97f897
PaulHaze <dev@artemist.com.au>
2024-09-09 18:25:24 +1000

    styles and slider added to categories

--- src/components/cards/CategoryCard.tsx
+++ src/components/cards/CategoryCard.tsx
@@ -9,22 +9,24 @@ type Props = {
 
 export function CategoryCard({ category }: Props) {
   return (
-    <div className="category-card group">
-      <div className="mb-6 overflow-hidden rounded-[15px]">
-        <Image
-          src={category.image}
-          alt="category image"
-          className="w-full transition-transform duration-300 ease-out group-hover:scale-110"
-        />
-      </div>
-      <div className="relative z-10 flex items-center justify-between">
-        <h4 className="uppercase">{category.title}</h4>
-        <Link
-          href={category.href}
-          className="rounded-full border border-stone-600 p-3 transition-all duration-1000 group-hover:bg-gradient-color"
-        >
-          <RxArrowRight size={32} />
-        </Link>
+    <div className="w-full flex-center">
+      <div className="category-card group w-[95%] sm:w-full">
+        <div className="mb-6 overflow-hidden rounded-[15px]">
+          <Image
+            src={category.image}
+            alt="category image"
+            className="w-full transition-transform duration-300 ease-out group-hover:scale-110"
+          />
+        </div>
+        <div className="relative z-10 flex items-center justify-between">
+          <h4 className="uppercase">{category.title}</h4>
+          <Link
+            href={category.href}
+            className="rounded-full border border-stone-600 p-3 transition-all duration-1000 group-hover:bg-gradient-color"
+          >
+            <RxArrowRight size={32} />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- src/components/sections/categories/CategoryBrowser.tsx
+++ src/components/sections/categories/CategoryBrowser.tsx
@@ -1,14 +1,44 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
+
 import { CategoryCard } from '@/components/cards';
 
 import { categories } from '@/data/category';
 
 export function CategoryBrowser() {
+  const swiperOptions = {
+    speed: 5000,
+    spaceBetween: 10,
+    loop: true,
+    autoplay: {
+      delay: 1,
+      pauseOnMouseEnter: true,
+    },
+    modules: [Autoplay],
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      576: { slidesPerView: 2 },
+      768: { slidesPerView: 2 },
+      992: { slidesPerView: 3 },
+      1200: { slidesPerView: 4 },
+      1500: { slidesPerView: 5 },
+      1700: { slidesPerView: 6, spaceBetween: 30 },
+    },
+  };
   return (
     <div className="mb-10">
       <h3 className="my-5 text-center uppercase">Browse by category</h3>
-      <div className="max-w-[350px]">
-        <CategoryCard category={categories[0]} />
-      </div>
+      {categories.length && (
+        <Swiper {...swiperOptions} className="swiper categories-slider">
+          {categories.map((category, index) => (
+            <SwiperSlide key={index}>
+              <CategoryCard category={category} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
     </div>
   );
 }
