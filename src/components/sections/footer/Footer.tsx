fbc45fb5a8dbdbec7425a6d75138cf8f2b8b80f2
PaulHaze <dev@artemist.com.au>
2024-09-10 06:30:41 +1000

    add logo to footer

--- src/components/sections/footer/Footer.tsx
+++ src/components/sections/footer/Footer.tsx
@@ -1,7 +1,33 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+import logo from '@/assets/images/logo.svg';
+
 export function Footer() {
   return (
-    <div className="bg-gradient-footer mt-8 rounded-t-2xl">
-      <p>Footer Placeholder</p>
+    <div className="bg-gradient-footer mt-[500px] rounded-t-3xl p-8">
+      <div className="grid grid-cols-1 gap-5">
+        {/* LOGO + ICONS */}
+        <div className="">
+          <Link href="/">
+            <Image src={logo} alt="logo" className="mb-4" />
+          </Link>
+          <p className="text-lg opacity-80">Connect with us!</p>
+          <div className="flex">ICONS</div>
+        </div>
+        <div className="cats">
+          <h5>CATS</h5>
+        </div>
+        <div className="help">
+          <h5>help</h5>
+        </div>
+        <div className="buttons">
+          <h5>buttons</h5>
+        </div>
+        <div className="privacy">
+          <h5>privacy</h5>
+        </div>
+      </div>
     </div>
   );
 }
